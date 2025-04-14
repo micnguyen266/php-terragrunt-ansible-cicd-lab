1f384df3d0f36aedf7c69d088fa6b04e4e5af879
Michael Nguyen <mrmikenguyen89@gmail.com>
2025-04-14 00:58:10 -0700

    setting workflow to manual

--- infrastructure/live/dev/asg/.terragrunt-cache/eyRV0Kf499ss8AX-XD2U2zb_Bak/1q0sBRgajD1f0DZxa7SRqHu0uFk/provider.tf
+++ infrastructure/live/dev/asg/.terragrunt-cache/eyRV0Kf499ss8AX-XD2U2zb_Bak/1q0sBRgajD1f0DZxa7SRqHu0uFk/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region  = var.region
-  profile = "default"  # if using a different profile name, specify it here
+#   profile = "default"  # if using a different profile name, specify it here
 }
